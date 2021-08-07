52ba1923fb46589260d3a66972486efd946bac2e
gomory-chvatal <gomory.chvatal@gmail.com>
2021-08-07 13:09:05 -0600

    add method to check for error

--- vtsapi/exceptions.py
+++ vtsapi/exceptions.py
@@ -1,2 +1,104 @@
-class NeedToAddExceptions(Exception):
-    """Exception description here."""
+class VTSInternalServerError(Exception):
+    """VTube Studio error 'InternalServerError'."""
+
+class VTSAPIAccessDeactivated(Exception):
+    """VTube Studio error 'APIAccessDeactivated'."""
+
+class VTSJSONInvalid(Exception):
+    """VTube Studio error 'JSONInvalid'."""
+
+class VTSAPINameInvalid(Exception):
+    """VTube Studio error 'APINameInvalid'."""
+
+class VTSAPIVersionInvalid(Exception):
+    """VTube Studio error 'APIVersionInvalid'."""
+
+class VTSRequestIDInvalid(Exception):
+    """VTube Studio error 'RequestIDInvalid'."""
+
+class VTSRequestTypeUnknown(Exception):
+    """VTube Studio error 'RequestTypeUnknown'."""
+
+class VTSRequestTypeMissingOrEmpty(Exception):
+    """VTube Studio error 'RequestTypeMissingOrEmpty'."""
+
+class VTSRequestRequiresAuthetication (Exception):
+    """VTube Studio error 'RequestRequiresAuthetication '."""
+
+class VTSTokenRequestDenied(Exception):
+    """VTube Studio error 'TokenRequestDenied'."""
+
+class VTSTokenRequestCurrentlyOngoing(Exception):
+    """VTube Studio error 'TokenRequestCurrentlyOngoing'."""
+
+class VTSTokenRequestPluginNameInvalid(Exception):
+    """VTube Studio error 'TokenRequestPluginNameInvalid'."""
+
+class VTSTokenRequestDeveloperNameInvalid(Exception):
+    """VTube Studio error 'TokenRequestDeveloperNameInvalid'."""
+
+class VTSTokenRequestPluginIconInvalid(Exception):
+    """VTube Studio error 'TokenRequestPluginIconInvalid'."""
+
+class VTSAuthenticationTokenMissing(Exception):
+    """VTube Studio error 'AuthenticationTokenMissing'."""
+
+class VTSAuthenticationPluginNameMissing(Exception):
+    """VTube Studio error 'AuthenticationPluginNameMissing'."""
+
+class VTSAuthenticationPluginDeveloperMissing(Exception):
+    """VTube Studio error 'AuthenticationPluginDeveloperMissing'."""
+
+class VTSModelIDMissing(Exception):
+    """VTube Studio error 'ModelIDMissing'."""
+
+class VTSModelIDInvalid(Exception):
+    """VTube Studio error 'ModelIDInvalid'."""
+
+class VTSModelIDNotFound(Exception):
+    """VTube Studio error 'ModelIDNotFound'."""
+
+class VTSModelLoadCooldownNotOver(Exception):
+    """VTube Studio error 'ModelLoadCooldownNotOver'."""
+
+class VTSCannotCurrentlyChangeModel(Exception):
+    """VTube Studio error 'CannotCurrentlyChangeModel'."""
+
+class VTSHotkeyQueueFull(Exception):
+    """VTube Studio error 'HotkeyQueueFull'."""
+
+class VTSHotkeyExecutionFailedBecauseNoModelLoaded(Exception):
+    """VTube Studio error 'HotkeyExecutionFailedBecauseNoModelLoaded'."""
+
+class VTSHotkeyIDNotFoundInModel(Exception):
+    """VTube Studio error 'HotkeyIDNotFoundInModel'."""
+
+class VTSHotkeyCooldownNotOver(Exception):
+    """VTube Studio error 'HotkeyCooldownNotOver'."""
+
+class VTSHotkeyIDFoundButHotkeyDataInvalid(Exception):
+    """VTube Studio error 'HotkeyIDFoundButHotkeyDataInvalid'."""
+
+class VTSHotkeyExecutionFailedBecauseBadState(Exception):
+    """VTube Studio error 'HotkeyExecutionFailedBecauseBadState'."""
+
+class VTSHotkeyUnknownExecutionFailure(Exception):
+    """VTube Studio error 'HotkeyUnknownExecutionFailure'."""
+
+class VTSColorTintRequestNoModelLoaded(Exception):
+    """VTube Studio error 'ColorTintRequestNoModelLoaded'."""
+
+class VTSColorTintRequestMatchOrColorMissing(Exception):
+    """VTube Studio error 'ColorTintRequestMatchOrColorMissing'."""
+
+class VTSColorTintRequestInvalidColorValue(Exception):
+    """VTube Studio error 'ColorTintRequestInvalidColorValue'."""
+
+class VTSMoveModelRequestNoModelLoaded(Exception):
+    """VTube Studio error 'MoveModelRequestNoModelLoaded'."""
+
+class VTSMoveModelRequestMissingFields(Exception):
+    """VTube Studio error 'MoveModelRequestMissingFields'."""
+
+class VTSMoveModelRequestValuesOutOfRange(Exception):
+    """VTube Studio error 'MoveModelRequestValuesOutOfRange'."""
--- vtsapi/response.py
+++ vtsapi/response.py
@@ -7,7 +7,9 @@ from vtsapi.exceptions import *
 
 
 class VTSResponse(object):
-    """Docstring goes here!"""
+    """Response object takes a JSON response from VTS, parses the JSON, and
+    raises any errors.
+    """
 
     def __init__(self, payload):
         
@@ -37,3 +39,98 @@ class VTSResponse(object):
     @property
     def data(self):
         return self._data
+
+    def __str__(self):
+        return f'response {self._id} {self._msg_type}:\n{self._payload}'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def check_error(self):
+        
+        if self._msg_type == 'APIError':
+            error_id = self._data['errorID']
+
+            # general errors
+            if error_id == 0:
+                raise VTSInternalServerError()
+            elif error_id == 1:
+                raise VTSAPIAccessDeactivated()
+            elif error_id == 2:
+                raise VTSJSONInvalid()
+            elif error_id == 3:
+                raise VTSAPINameInvalid()
+            elif error_id == 4:
+                raise VTSAPIVersionInvalid()
+            elif error_id == 5:
+                raise VTSRequestIDInvalid()
+            elif error_id == 6:
+                raise VTSRequestTypeUnknown()
+            elif error_id == 7:
+                raise VTSRequestTypeMissingOrEmpty()
+            elif error_id == 8:
+                raise VTSRequestRequiresAuthetication()
+
+            # token request related errors
+            elif error_id == 50:
+                raise VTSTokenRequestDenied()
+            elif error_id == 51:
+                raise VTSTokenRequestCurrentlyOngoing()
+            elif error_id == 52:
+                raise VTSTokenRequestPluginNameInvalid()
+            elif error_id == 53:
+                raise VTSTokenRequestDeveloperNameInvalid()
+            elif error_id == 54:
+                raise VTSTokenRequestPluginIconInvalid()
+
+            # authentication request related errors
+            elif error_id == 100:
+                raise VTSAuthenticationTokenMissing()
+            elif error_id == 101:
+                raise VTSAuthenticationPluginNameMissing()
+            elif error_id == 102:
+                raise VTSAuthenticationPluginDeveloperMissing()
+
+            # model load related errors
+            elif error_id == 150:
+                raise VTSModelIDMissing()
+            elif error_id == 151:
+                raise VTSModelIDInvalid()
+            elif error_id == 152:
+                raise VTSModelIDNotFound()
+            elif error_id == 153:
+                raise VTSModelLoadCooldownNotOver()
+            elif error_id == 154:
+                raise VTSCannotCurrentlyChangeModel()
+
+            # hotkey trigger request related errors
+            elif error_id == 200:
+                raise VTSHotkeyQueueFull()
+            elif error_id == 201:
+                raise VTSHotkeyExecutionFailedBecauseNoModelLoaded()
+            elif error_id == 202:
+                raise VTSHotkeyIDNotFoundInModel()
+            elif error_id == 203:
+                raise VTSHotkeyCooldownNotOver()
+            elif error_id == 204:
+                raise VTSHotkeyIDFoundButHotkeyDataInvalid()
+            elif error_id == 205:
+                raise VTSHotkeyExecutionFailedBecauseBadState()
+            elif error_id == 206:
+                raise VTSHotkeyUnknownExecutionFailure()
+
+            # color tint request related errors
+            elif error_id == 250:
+                raise VTSColorTintRequestNoModelLoaded()
+            elif error_id == 251:
+                raise VTSColorTintRequestMatchOrColorMissing()
+            elif error_id == 252:
+                raise VTSColorTintRequestInvalidColorValue()
+
+            # model move request related errors
+            elif error_id == 300:
+                raise VTSMoveModelRequestNoModelLoaded()
+            elif error_id == 301:
+                raise VTSMoveModelRequestMissingFields()
+            elif error_id == 302:
+                raise VTSMoveModelRequestValuesOutOfRange()        
